0324c1767a5e1ab0b3f39e33888a9e46f635e0cf
진수 <moonlt93@naver.com>
2025-01-05 14:02:36 +0900

    chore: concert domain추가

---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/concert/application/domain/model/ConcertSchedule.kt
@@ -0,0 +1,3 @@
+package com.my.sparta.concert.aggregate.concert.application.domain.model
+
+class ConcertSchedule
---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/concert/application/domain/valueobject/HallType.kt
@@ -0,0 +1,7 @@
+package com.my.sparta.concert.aggregate.concert.application.domain.valueobject
+
+enum class HallType {
+    COMMON,
+    BUSINESS,
+    VIP
+}
