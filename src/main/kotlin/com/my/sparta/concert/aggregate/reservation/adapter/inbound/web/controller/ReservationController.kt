77441e5fe088c99f462d10cab58c9955cdf0628f
진수 <moonlt93@naver.com>
2025-01-05 14:05:39 +0900

    chore: controller 별 todo 작성

---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/reservation/adapter/inbound/web/controller/ReservationController.kt
@@ -0,0 +1,17 @@
+package com.my.sparta.concert.aggregate.reservation.adapter.inbound.web.controller
+
+import lombok.extern.slf4j.Slf4j
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@RestController
+@RequestMapping("/reservation")
+class ReservationController {
+
+    @PostMapping
+    fun reserveConcert() {
+        //TODO: 유저가 좌석을 조회하고  콘서트를 예약한다.
+    }
+}
