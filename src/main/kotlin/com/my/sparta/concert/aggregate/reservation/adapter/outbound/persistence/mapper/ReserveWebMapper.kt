b9e891d350a86ad735e8ebecfebf8da5387e3b5b
진수 <moonlt93@naver.com>
2025-01-09 07:18:12 +0900

    feat: 콘서트 예매 내역 save

---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/reservation/adapter/outbound/persistence/mapper/ReserveWebMapper.kt
@@ -0,0 +1,22 @@
+package com.my.sparta.concert.aggregate.reservation.adapter.outbound.persistence.mapper
+
+import com.my.sparta.concert.aggregate.reservation.adapter.inbound.web.interfaces.ConcertReservationRequest
+import com.my.sparta.concert.aggregate.reservation.application.port.inbound.command.ConcertReservationCommand
+import com.my.sparta.concert.aggregate.user.application.domain.valueobject.PaymentType
+import org.springframework.stereotype.Component
+
+@Component
+class ReserveWebMapper {
+
+    fun mapToCommand(request: ConcertReservationRequest): ConcertReservationCommand {
+        return ConcertReservationCommand(
+            request.concertId,
+            request.userId,
+            request.concertScheduleId,
+            request.concertSeatNumber,
+            request.count,
+            PaymentType.valueOf(request.paymentType),
+        )
+    }
+
+}
