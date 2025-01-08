e3f2a25399c4462033223107affa1e4937136f6d
진수 <moonlt93@naver.com>
2025-01-09 07:17:05 +0900

    feat: facade 계층 구분 및 port를 활용한 비지니스 로직 구성

---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/reservation/application/port/outbound/SavePaymentHistoryPort.kt
@@ -0,0 +1,9 @@
+package com.my.sparta.concert.aggregate.reservation.application.port.outbound
+
+import com.my.sparta.concert.aggregate.user.application.domain.model.Payment
+
+interface SavePaymentHistoryPort {
+
+    fun savePaymentInfo(payment: Payment): Payment
+
+}
