563f699954d2f58632299513d8a338a020315d9e
진수 <moonlt93@naver.com>
2025-01-10 05:06:10 +0900

    chore: 경로 수정으로 인한 변경

--- src/main/kotlin/com/my/sparta/concert/aggregate/user/application/scheduler/ExpiredTokenDiscardService.kt
+++ src/main/kotlin/com/my/sparta/concert/aggregate/user/application/scheduler/ExpiredTokenDiscardService.kt
@@ -2,7 +2,7 @@ package com.my.sparta.concert.aggregate.user.application.scheduler
 
 import com.my.sparta.concert.aggregate.user.application.port.outbound.DeleteQueueingTokenPort
 import com.my.sparta.concert.aggregate.user.application.port.outbound.LoadQueueingTokenPort
-import com.my.sparta.concert.aggregate.user.application.port.outbound.SaveQueueingTokenPort
+import com.my.sparta.concert.aggregate.user.application.scheduler.usecase.HandlingTokenUseCase
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
 import org.springframework.scheduling.annotation.Scheduled
@@ -12,23 +12,10 @@ import org.springframework.transaction.annotation.Transactional
 @Slf4j
 @Service
 @RequiredArgsConstructor
-class TokenQueueingService(
+class ExpiredTokenDiscardService(
     private val loadQueueingTokenPort: LoadQueueingTokenPort,
-    private val saveQueueingTokenPort: SaveQueueingTokenPort,
     private val deleteQueueingTokenPort: DeleteQueueingTokenPort
 ) : HandlingTokenUseCase {
-
-    @Scheduled(cron = "0 */1 * * * ?")
-    @Transactional
-    override fun changeActiveTokens() {
-
-        val tokens = loadQueueingTokenPort.loadActivatableTokens();
-        for (token in tokens) {
-            token.isActive = true;
-        }
-        saveQueueingTokenPort.saveTokens(tokens);
-
-    }
 
     @Scheduled(cron = "30 */1 * * * ?")
     @Transactional
--- src/main/kotlin/com/my/sparta/concert/aggregate/user/application/scheduler/HandlingTokenUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.my.sparta.concert.aggregate.user.application.scheduler
-
-interface HandlingTokenUseCase {
-
-    fun changeActiveTokens();
-
-    fun discardExpiredTokens();
-
-}
