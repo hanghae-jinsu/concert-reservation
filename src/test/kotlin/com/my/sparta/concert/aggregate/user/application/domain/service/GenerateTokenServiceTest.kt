778a1261813c55051f10661c5646ec0ca7b063f3
진수 <moonlt93@naver.com>
2025-01-10 07:01:35 +0900

    test: service 계층 test 작성

---
+++ src/test/kotlin/com/my/sparta/concert/aggregate/user/application/domain/service/GenerateTokenServiceTest.kt
@@ -0,0 +1,49 @@
+package com.my.sparta.concert.aggregate.user.application.domain.service
+
+import com.my.sparta.concert.aggregate.user.application.domain.model.UserToken
+import com.my.sparta.concert.aggregate.user.application.domain.model.Users
+import com.my.sparta.concert.aggregate.user.application.port.inbound.GenerateTokenUseCase
+import com.my.sparta.concert.aggregate.user.application.port.outbound.LoadUserInfoPort
+import com.my.sparta.concert.aggregate.user.application.port.outbound.SaveUserTokenPort
+import com.my.sparta.concert.common.util.TokenUtilService
+import io.kotest.assertions.any
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class GenerateTokenServiceTest {
+    private val loadUserInfoPort: LoadUserInfoPort = mockk()
+    private val saveUserTokenPort: SaveUserTokenPort = mockk()
+    private val tokenUtilService: TokenUtilService = mockk()
+    private val service: GenerateTokenUseCase = GenerateTokenService(
+        loadUserInfoPort = loadUserInfoPort,
+        saveUserTokenPort = saveUserTokenPort,
+        tokenUtilService = tokenUtilService
+    )
+
+    @Test
+    fun `generateToken should load user info, generate token, and save it`() {
+        // Arrange
+        val userId = "user123"
+        val userInfo = Users(userId = userId,"username",10, mockk())
+        val generatedToken = "generatedToken123"
+        val savedTokenId = "savedTokenId123"
+        every { loadUserInfoPort.getUserInfoById(userId) } returns userInfo
+        every { tokenUtilService.generateToken(userId) } returns UserToken(
+            tokenId = generatedToken,
+            userId = userId,
+            isActive = false,
+            createdAt = LocalDateTime.now(),
+            expiresAt = LocalDateTime.now().plusMinutes(5)
+        )
+        every { saveUserTokenPort.saveUserToken(any()) } returns savedTokenId
+
+        // Act
+        val result = service.generateToken(userId)
+
+        // Assert
+        assertEquals(savedTokenId, result)
+    }
+}
---
+++ src/test/kotlin/com/my/sparta/concert/aggregate/user/application/domain/service/UserChargeMoneyServiceTest.kt
@@ -0,0 +1,57 @@
+package com.my.sparta.concert.aggregate.user.application.domain.service
+
+import com.my.sparta.concert.aggregate.user.application.domain.model.Users
+import com.my.sparta.concert.aggregate.user.application.domain.valueobject.PaymentType
+import com.my.sparta.concert.aggregate.user.application.domain.valueobject.Wallet
+import com.my.sparta.concert.aggregate.user.application.port.inbound.command.UserChargeCommand
+import com.my.sparta.concert.aggregate.user.application.port.outbound.LoadUserInfoPort
+import com.my.sparta.concert.aggregate.user.application.port.outbound.SaveMoneyPort
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class UserChargeMoneyServiceTest {
+
+    private val saveMoneyPort: SaveMoneyPort = mockk()
+    private val loadUserInfoPort: LoadUserInfoPort = mockk()
+    private val service = UserChargeMoneyService(saveMoneyPort, loadUserInfoPort)
+
+    @Test
+    fun `chargeMoney should load user info, charge money, and save user`() {
+        // Arrange
+        val userId = "user123"
+        val initialMoney = 1000
+        val chargeAmount = 1000
+        val command = UserChargeCommand(
+            userId = userId,
+            wallet = Wallet(PaymentType.CARD,money = 1000.0)
+        )
+
+        val userInfo = Users(
+            userId = userId,
+            username = "Test User",
+            age = 12,
+            wallet = Wallet(PaymentType.CARD,money = 1000.0)
+        )
+
+        val updatedUserInfo = Users(
+            userId = userId,
+            username = "Test User",
+            age = 15,
+            wallet = Wallet(PaymentType.CARD,money = 2000.0)
+        )
+
+        every { loadUserInfoPort.getUserInfoById(userId) } returns userInfo
+        every { saveMoneyPort.saveMoney(userInfo) } returns updatedUserInfo
+
+        // Act
+        val result = service.chargeMoney(command)
+
+        // Assert
+        assertEquals(userId, result.userId)
+        assertEquals(initialMoney + chargeAmount, result.wallet.money.toInt())
+
+    }
+
+}
