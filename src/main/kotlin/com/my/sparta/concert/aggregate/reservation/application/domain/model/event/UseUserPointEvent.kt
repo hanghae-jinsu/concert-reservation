c92cff1a54e65b3e6f58876af0a77eb272840206
진수 <moonlt93@naver.com>
2025-01-22 22:05:56 +0900

    feat: 유저 포인트 차감 이벤트 발행 및 listener 작성

--- src/main/kotlin/com/my/sparta/concert/aggregate/reservation/application/domain/model/event/UseUserPointEvent.kt
+++ src/main/kotlin/com/my/sparta/concert/aggregate/reservation/application/domain/model/event/UseUserPointEvent.kt
@@ -1,7 +1,7 @@
 package com.my.sparta.concert.aggregate.reservation.application.domain.model.event
 
-data class ChangeStatusUseSeatEvent(
-    val seatId: Int,
+data class UseUserPointEvent(
     val userId: String,
+    val totalPrice: Double,
 ) {
 }
