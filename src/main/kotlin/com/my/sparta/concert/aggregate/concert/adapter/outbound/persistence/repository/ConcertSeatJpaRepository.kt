54d54f96bb52cea1065f277201bd387084380524
진수 <moonlt93@naver.com>
2025-01-22 22:07:37 +0900

    feat: 만료시간 지나기전에 예약이 가능하도록 로직 수정

--- src/main/kotlin/com/my/sparta/concert/aggregate/concert/adapter/outbound/persistence/repository/ConcertSeatJpaRepository.kt
+++ src/main/kotlin/com/my/sparta/concert/aggregate/concert/adapter/outbound/persistence/repository/ConcertSeatJpaRepository.kt
@@ -1,7 +1,6 @@
 package com.my.sparta.concert.aggregate.concert.adapter.outbound.persistence.repository
 
 import com.my.sparta.concert.aggregate.concert.adapter.outbound.persistence.entity.ConcertSeatEntity
-import com.my.sparta.concert.aggregate.concert.application.domain.model.ConcertSchedule
 import com.my.sparta.concert.aggregate.concert.application.domain.model.ConcertSeat
 import jakarta.persistence.*
 import org.springframework.data.jpa.repository.JpaRepository
@@ -13,26 +12,28 @@ import java.util.*
 
 interface ConcertSeatJpaRepository : JpaRepository<ConcertSeatEntity, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(
         """
     select cs 
     from ConcertSeatEntity as cs 
     where cs.concertSeatId IN :seatId 
       and cs.concertSchedule.concertScheduleId = :scheduleId
-      and cs.seatStatus = :seatStatus
+      and cs.seatStatus in :seatStatus
 """,
     )
-//    @QueryHints(
-//        QueryHint(
-//            name = "jakarta.persistence.lock.timeout",
-//            value = "1500"
-//        )
-//    )
+    @QueryHints(
+        QueryHint(
+            name = "jakarta.persistence.lock.timeout",
+            value = "1500"
+        )
+    )
     fun findByIdAndScheduleId(
         @Param("seatId") seatId: Int,
         @Param("scheduleId") scheduleId: String,
-        @Param("seatStatus") seatStatus: ConcertSeat.SeatStatus
+        @Param("seatStatus") seatStatus: List<ConcertSeat.SeatStatus>
     ): Optional<ConcertSeatEntity>
 
+
+    @Query("select cs from ConcertSeatEntity cs where cs.concertSeatId = :seatId and cs.seatStatus = :status")
+    fun findByIdWithStatus(seatId: Long, status: ConcertSeat.SeatStatus): ConcertSeatEntity
 }
