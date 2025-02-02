5ac80f108449eaac61ccba45cf24370f3cca542a
진수 <moonlt93@naver.com>
2025-02-02 10:10:10 +0900

    test: lint 수정으로 인한 변경 2

--- src/test/kotlin/com/my/sparta/concert/aggregate/user/adapter/inbound/controller/UserQueryControllerTest.kt
+++ src/test/kotlin/com/my/sparta/concert/aggregate/user/adapter/inbound/controller/UserQueryControllerTest.kt
@@ -1,8 +1,8 @@
 package com.my.sparta.concert.aggregate.user.adapter.inbound.controller
 
+import com.my.sparta.concert.aggregate.user.adapter.inbound.web.controller.UserQueryController
 import com.my.sparta.concert.aggregate.user.adapter.inbound.web.interfaces.response.UserWalletInfoResponse
 import com.my.sparta.concert.aggregate.user.adapter.inbound.web.mapper.UserWebMapper
-import com.my.sparta.concert.aggregate.user.adapter.inbound.web.controller.UserQueryController
 import com.my.sparta.concert.aggregate.user.application.domain.model.Users
 import com.my.sparta.concert.aggregate.user.application.domain.valueobject.PaymentType
 import com.my.sparta.concert.aggregate.user.application.domain.valueobject.Wallet
@@ -19,11 +19,11 @@ import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import java.math.BigDecimal
 
 @WebMvcTest(controllers = [UserQueryController::class])
 class UserQueryControllerTest(
     @Autowired val mockMvc: MockMvc,
-
     // MockBean을 사용하여 Spring 컨텍스트에서 의존성을 모킹
     @MockBean val generateTokenUseCase: GenerateTokenUseCase,
     @MockBean val loadUserInfoPort: LoadUserInfoPort,
@@ -37,27 +37,30 @@ class UserQueryControllerTest(
         val userId = "user12345"
 
         // 모킹할 사용자 정보 객체 생성
-        val mockUserInfo = Users(
-            userId = userId,
-            username = "user",
-            age = 20,
-            wallet = Wallet(PaymentType.CARD, 5000.0)
-        )
+        val mockUserInfo =
+            Users(
+                userId = userId,
+                username = "user",
+                age = 20,
+                wallet = Wallet(PaymentType.CARD, BigDecimal(5000.0)),
+            )
 
         // 모킹할 UserWalletInfoResponse 생성
-        val mockUserWalletInfoResponse = UserWalletInfoResponse(
-            userId = userId,
-            money = 5000.0
-        )
+        val mockUserWalletInfoResponse =
+            UserWalletInfoResponse(
+                userId = userId,
+                money = BigDecimal(5000.0),
+            )
 
         // 의존성 모킹 설정
         every { loadUserInfoPort.getUserInfoById(userId) } returns mockUserInfo
         every { userWebMapper.mapToResponse(mockUserInfo) } returns mockUserWalletInfoResponse
 
         When("GET /user/money/{userId} 요청을 보내면") {
-            val response = mockMvc.get("/user/money/{userId}", userId) {
-                contentType = MediaType.APPLICATION_JSON
-            }
+            val response =
+                mockMvc.get("/user/money/{userId}", userId) {
+                    contentType = MediaType.APPLICATION_JSON
+                }
 
             Then("응답 상태는 200 OK이고, 잔고 정보가 반환되어야 한다") {
                 response.andExpect {
@@ -77,9 +80,10 @@ class UserQueryControllerTest(
         every { loadUserInfoPort.getUserInfoById(invalidUserId) } throws EntityNotFoundException("User not found")
 
         When("GET /user/money/{userId} 요청을 보내면") {
-            val response = mockMvc.get("/user/money/{userId}", invalidUserId) {
-                contentType = MediaType.APPLICATION_JSON
-            }
+            val response =
+                mockMvc.get("/user/money/{userId}", invalidUserId) {
+                    contentType = MediaType.APPLICATION_JSON
+                }
 
             Then("응답 상태는 404 Not Found를 반환해야 한다") {
                 response.andExpect {
@@ -88,6 +92,4 @@ class UserQueryControllerTest(
             }
         }
     }
-
-
 })
