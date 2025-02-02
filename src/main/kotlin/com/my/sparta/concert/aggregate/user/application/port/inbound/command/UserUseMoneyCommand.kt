980d80d423e7c529e436c56e21d46c7c09595c6b
진수 <moonlt93@naver.com>
2025-02-02 10:10:53 +0900

    test: DB 격리 레벨에 따른 test 작성

---
+++ src/main/kotlin/com/my/sparta/concert/aggregate/user/application/port/inbound/command/UserUseMoneyCommand.kt
@@ -0,0 +1,9 @@
+package com.my.sparta.concert.aggregate.user.application.port.inbound.command
+
+import java.math.BigDecimal
+
+data class UserUseMoneyCommand(
+
+    val userId: String,
+    val paidAmount: BigDecimal,
+)
