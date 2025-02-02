81994d9f0073eb1cfb9a31437b5087d2c6120cd0
진수 <moonlt93@naver.com>
2025-02-02 10:07:48 +0900

    test: yaml 통합 수정

--- src/test/kotlin/com/my/sparta/concert/TestcontainersConfiguration.kt
+++ src/test/kotlin/com/my/sparta/concert/TestcontainersConfiguration.kt
@@ -1,33 +1,39 @@
 package com.my.sparta.concert
 
 import jakarta.annotation.PreDestroy
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Configuration
 import org.testcontainers.containers.MySQLContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.utility.DockerImageName
 
 @Configuration
 class TestcontainersConfiguration {
-
     @PreDestroy
     fun preDestroy() {
         if (mySqlContainer.isRunning) mySqlContainer.stop()
     }
 
     companion object {
-
+        @Container
+        @ServiceConnection
         val mySqlContainer: MySQLContainer<*> =
-            MySQLContainer(DockerImageName.parse("mysql:latest"))
+
+            MySQLContainer(DockerImageName.parse("mysql:8.0"))
                 .withDatabaseName("concert")
                 .withUsername("users")
                 .withPassword("password")
                 .withInitScript("static/data/concert_script.sql")
+                .waitingFor(Wait.forHttp("/"))                          // 가용가능 한지 기다렸다가
+                .withReuse(true)
                 .apply {
-                    start()
-                }
+                            start()
+                        }
 
         init {
             System.setProperty(
-               "spring.datasource.url",
+                "spring.datasource.url",
                 mySqlContainer.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC",
             )
             System.setProperty("spring.datasource.username", mySqlContainer.username)
--- src/test/kotlin/com/my/sparta/concert/aggregate/RedisTestConfig.kt
+++ src/test/kotlin/com/my/sparta/concert/aggregate/RedisTestConfig.kt
@@ -5,7 +5,6 @@ import org.redisson.api.RedissonClient
 import org.redisson.config.Config
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.core.ReactiveRedisTemplate
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
@@ -15,12 +14,12 @@ import org.testcontainers.containers.GenericContainer
 
 @TestConfiguration
 class RedisTestConfig {
-
     companion object {
-        private val REDIS_CONTAINER = GenericContainer<Nothing>("redis:5.0.3-alpine").apply {
-            withExposedPorts(6379)
-            start()
-        }
+        private val REDIS_CONTAINER =
+            GenericContainer<Nothing>("redis:5.0.3-alpine").apply {
+                withExposedPorts(6379)
+                start()
+            }
 
         val redisHost: String
             get() = REDIS_CONTAINER.host
@@ -28,21 +27,24 @@ class RedisTestConfig {
         val redisPort: Int
             get() = REDIS_CONTAINER.getMappedPort(6379)
     }
-//    @Bean
-//    fun reactiveRedisTemplate(reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, Any> {
-//        val serializer = Jackson2JsonRedisSerializer(Any::class.java)
-//        val builder = RedisSerializationContext.newSerializationContext<String, Any>(StringRedisSerializer())
-//        val context = builder.value(serializer).build()
-//
-//        return ReactiveRedisTemplate(reactiveRedisConnectionFactory, context)
-//    }
+
+    @Bean
+    fun reactiveRedisTemplate(reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, Any> {
+        val serializer = Jackson2JsonRedisSerializer(Any::class.java)
+        val builder = RedisSerializationContext.newSerializationContext<String, Any>(StringRedisSerializer())
+        val context = builder.value(serializer).build()
+
+        return ReactiveRedisTemplate(reactiveRedisConnectionFactory, context)
+    }
+
     @Bean
     fun redissonClient(): RedissonClient {
-        val config = Config().apply {
-            useSingleServer().apply {
-                address = "redis://$redisHost:$redisPort"
+        val config =
+            Config().apply {
+                useSingleServer().apply {
+                    address = "redis://$redisHost:$redisPort"
+                }
             }
-        }
         return Redisson.create(config)
     }
 }
